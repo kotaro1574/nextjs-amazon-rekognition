9b7e43e8afc66b8f0f00ffd484520cdc03517ee1
kotaro1574 <shake1574@gmail.com>
2024-03-13 11:56:32 -0700

    fix

--- app/index-faces/page.tsx
+++ app/index-faces/page.tsx
@@ -1,6 +1,6 @@
 import { IndexFacesForm } from "./index-faces-form"
 
-export default function UploadPage() {
+export default function IndexFacesPage() {
   return (
     <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
       <div className="flex max-w-[980px] flex-col items-start gap-2">

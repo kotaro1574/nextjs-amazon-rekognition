a15a9979e8950b3ea1cf2cc69ce6f9e6c4a5462b
kotaro1574 <shake1574@gmail.com>
2024-03-13 11:12:53 -0700

    create: search-faces

--- app/search-faces/page.tsx
+++ app/search-faces/page.tsx
@@ -1,15 +1,12 @@
-import Link from "next/link"
-
-export default function IndexPage() {
+export default function SearchFacesPage() {
   return (
     <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
       <div className="flex max-w-[980px] flex-col items-start gap-2">
         <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
-          Amazon Rekognition
+          Search Faces
         </h1>
       </div>
-      <Link href="/index-faces">index faces</Link>
-      <Link href="/search-faces">search faces</Link>
+      <div></div>
     </section>
   )
 }
---
+++ app/search-faces/search-faces-form.tsx
@@ -0,0 +1,93 @@
+"use client"
+
+import { startTransition, useState } from "react"
+import Image from "next/image"
+import { useRouter } from "next/navigation"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { Controller, useForm } from "react-hook-form"
+import { z } from "zod"
+
+import { Button, buttonVariants } from "@/components/ui/button"
+import { Form, FormItem, FormLabel } from "@/components/ui/form"
+
+const formSchema = z.object({
+  imageFile: z.custom<File>().nullable(),
+})
+
+export function SearchFacesForm() {
+  const [uploading, setUploading] = useState(false)
+  const router = useRouter()
+  const form = useForm<z.infer<typeof formSchema>>({
+    defaultValues: {
+      imageFile: null,
+    },
+    resolver: zodResolver(formSchema),
+  })
+
+  const onSubmit = async (data: z.infer<typeof formSchema>) => {
+    setUploading(true)
+    try {
+    } catch (error) {
+      console.error("Error submitting form:", error)
+      alert("Failed to index face due to a network or other error")
+    } finally {
+      setUploading(false)
+    }
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+        <Controller
+          render={({ field: { onChange, value } }) => (
+            <FormItem>
+              <FormLabel htmlFor="imageFile">Image</FormLabel>
+              {value && (
+                <Image
+                  src={URL.createObjectURL(value)}
+                  alt="Uploaded Image"
+                  width={200}
+                  height={200}
+                />
+              )}
+              <div style={{ width: "200px" }} className="relative">
+                <label
+                  className={`${buttonVariants({
+                    variant: "default",
+                    size: "default",
+                  })} mt-2 block w-full`}
+                  htmlFor="single"
+                >
+                  {uploading ? "Uploading ..." : "Upload"}
+                </label>
+
+                <input
+                  style={{
+                    visibility: "hidden",
+                    position: "absolute",
+                    width: 0,
+                  }}
+                  type="file"
+                  id="single"
+                  accept="image/*"
+                  onChange={(e) => {
+                    if (!e.target.files?.[0]) return
+                    setUploading(true)
+                    onChange(e.target.files?.[0] as File)
+                    setUploading(false)
+                  }}
+                  disabled={uploading}
+                />
+              </div>
+            </FormItem>
+          )}
+          name="imageFile"
+          control={form.control}
+        />
+        <Button disabled={uploading} type="submit">
+          {uploading ? "loading..." : "index face"}
+        </Button>
+      </form>
+    </Form>
+  )
+}

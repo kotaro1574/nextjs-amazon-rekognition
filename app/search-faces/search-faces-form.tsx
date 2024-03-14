cf3cd0539e03e6e5ee1de68f30bc26148bdffe4b
kotaro1574 <shake1574@gmail.com>
2024-03-14 16:25:41 -0700

    delete: 不要import発見

--- app/search-faces/search-faces-form.tsx
+++ app/search-faces/search-faces-form.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { startTransition, useState } from "react"
+import { useState } from "react"
 import Image from "next/image"
 import { useRouter } from "next/navigation"
 import { zodResolver } from "@hookform/resolvers/zod"
